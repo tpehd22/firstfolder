e6a500b4b3501d77b1cc1c8d083067a770e3d463
tpehd22 <tpehd22@naver.com>
2021-06-30 22:02:12 +0900

    asd

---
+++ 1.js
@@ -0,0 +1,25 @@
+/*// console.log("hello world"); // printf
+var i = 10;
+if(i>10){
+    console.log("10보다 작다");
+}
+else{
+    console.log("작다");
+}*/
+
+// for(var i = 0; i<10; i++){
+//     console.log("*");
+// }
+
+// var i = 0;
+// while(i <10){
+//     i++;
+//     console.log(i);
+// }
+
+var arr = new Array(3);
+arr[0] = "adfaf";
+arr[1] = "adff";
+arr[2] = "ad";
+// var arr = ["adfa", "adfadsf", "adf"];
+console.log(arr);
